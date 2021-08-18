1640a5c9d5622ba27521dfccae7a1d2f58440388
Your Name <aeash.pouya.c@gmail.com>
2021-08-18 20:13:55 +0430

    master

--- src/pages/Cardpage.js
+++ src/pages/Cardpage.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "../pages/Cardpage.css";
+import Card from "../component/Card";
 const Cardpage = (props) => {
   return (
     <React.Fragment>
       <div className="Cardpage">
-        <p>product example</p>
-        <div className="card">
-          {/* <img src={this.props.img} /> */}
-          <div className="card-body">
-            <h2>{}</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-            <h5>{}</h5>
-          </div>
-          <button className="buy">asdasd</button>
-        </div>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </React.Fragment>
   );
